7b2efae348480dfb92adfb5e6944e57220eafe64
gladysronquillo <gladysronquillo@outlook.com>
2018-01-08 17:44:34 -0500

    Create Ecuavinos_produccion.sql

---
+++ Ecuavinos_produccion.sql
@@ -0,0 +1,112 @@
+USE [master]
+GO
+
+/****** Object:  Database [Ecuavinos_produccion]    Script Date: 1/8/2018 10:33:59 PM ******/
+CREATE DATABASE [Ecuavinos_produccion]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'Ecuavinos', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\Ecuavinos.mdf' , SIZE = 4096KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'Ecuavinos_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\Ecuavinos_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET COMPATIBILITY_LEVEL = 120
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [Ecuavinos_produccion].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET TARGET_RECOVERY_TIME = 0 SECONDS 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [Ecuavinos_produccion] SET  READ_WRITE 
+GO
+
+
